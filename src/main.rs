2b7855c7e633779661ee6d45d493ac6032032212
torressam333 <torressam333@gmail.com>
2026-01-08 08:36:30 -0800

    Move main functionality to lib and use main as binary entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,6 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello, {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use newsletter_service::run;
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8005")?
-    .run()
-    .await
+async fn main() -> Result<(), std::io::Error> {
+    run().await
 }
