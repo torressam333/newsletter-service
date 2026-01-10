b9e1979f26b7da62dd5d87e1f7f03f807059920e
torressam333 <torressam333@gmail.com>
2026-01-10 09:11:26 -0800

    Add tdd test for the different 400 missing data calls to sub endpoint

--- tests/health_checks.rs
+++ tests/health_checks.rs
@@ -46,3 +46,33 @@ async fn subuscribe_returns_a_200_for_valid_form_data() {
 
     assert_eq!(200, response.status().as_u16());
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("emaik=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Add customized message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
