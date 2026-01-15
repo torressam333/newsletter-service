58599303a909e45d3cae86e7eaaed3c4b919ed22
torressam333 <torressam333@gmail.com>
2026-01-15 07:55:45 -0800

    Begin adding logic to read values from a config yaml file

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,1 +1,28 @@
+#[derive(serde::Deserisalize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
 
+#[derive(serde::Deserisalize, clone)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Init the config yaml reader
+    let settings = config::Config::builder()
+        // Add config values from specific config yaml file
+        .add_source(config::File::new(
+            "configuration.yaml",
+            config::FileFormat::Yaml,
+        ))
+        .build()?;
+
+    // Try to convert the read config values into the Settings type
+    settings.try_deserialize::<Settings>()
+}
