0e2caed34163782978c4fef74e001b347bbb22ee
torressam333 <torressam333@gmail.com>
2026-01-16 10:34:44 -0800

    Going to use sqlx prepare for offline db to not have to run a live db everytime

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
 pub mod configuration;
 pub mod routes;
 pub mod startup;
+
+// Put this anywhere in src/lib.rs
+async fn _dummy_query(pool: sqlx::PgPool) {
+    sqlx::query!("SELECT id FROM subscriptions")
+        .fetch_one(&pool)
+        .await
+        .ok();
+}
