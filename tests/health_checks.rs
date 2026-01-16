78f21cd631ddab48871707bb93fbb998b385b755
torressam333 <torressam333@gmail.com>
2026-01-16 08:17:44 -0800

    WIP: Add some logic to asset db persistence for 200 subscription check

--- tests/health_checks.rs
+++ tests/health_checks.rs
@@ -1,3 +1,5 @@
+use newsletter_service::configuration::get_configuration;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 #[tokio::test]
@@ -32,6 +34,14 @@ fn spawn_app() -> String {
 #[tokio::test]
 async fn subuscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to get configuration.");
+    let connection_string = configuration.database.connection_string();
+
+    // We have to manually bring Connection trait into scope for pg connection to work
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres DB");
+
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -45,6 +55,14 @@ async fn subuscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch savved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
