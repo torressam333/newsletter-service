56238d4d3f7be0f54a9a50de68eb7c1a0599d9d0
torressam333 <torressam333@gmail.com>
2026-01-18 09:02:16 -0800

    Begin swapping single pg conn for a PgPool for concurrency setup

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
 use actix_web::{HttpResponse, web};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,6 +9,20 @@ pub struct FormData {
     pub name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgPool>) -> HttpResponse {
+    let _ = sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // Use get_ref to gen an immutable ref to the PgConnection wrapped in web::Data
+    .execute(connection.get_ref())
+    .await;
+
     HttpResponse::Ok().finish()
 }
