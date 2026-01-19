ebb8016253e186dcbf85677e48d85ea5fb12385c
torressam333 <torressam333@gmail.com>
2026-01-19 09:20:30 -0800

    Add new struct to package our db address and pool together

--- tests/health_checks.rs
+++ tests/health_checks.rs
@@ -2,13 +2,18 @@ use newsletter_service::configuration::get_configuration;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
+pub struct TestApp {
+    pub address: String,
+    pub db_pool: PgPool,
+}
+
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app().await;
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let response = client
-        .get(format!("{}/health_check", address))
+        .get(format!("{}/health_check", app.address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -25,39 +30,36 @@ I make my test setup async because initializing the database connection is an as
 While it adds a tiny bit of boilerplate to each test call, it keeps the setup logic
 honest—I am performing I/O before the test starts, so the function signature should reflect that
 */
-async fn spawn_app() -> String {
+async fn spawn_app() -> TestApp {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
+    let address = format!("http://127.0.0.1:{}", port);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
         .expect("Failed to connect to postgres");
-    let server =
-        newsletter_service::startup::run(listener, connection).expect("Failed to bind address");
+
+    let server = newsletter_service::startup::run(listener, connection_pool.clone())
+        .expect("Failed to bind address");
 
     tokio::spawn(server);
 
-    format!("http://127.0.0.1:{}", port)
+    TestApp {
+        address,
+        db_pool: connection_pool,
+    }
 }
 
 #[tokio::test]
 async fn subuscribe_returns_a_200_for_valid_form_data() {
-    let app_address = spawn_app().await;
-    let configuration = get_configuration().expect("Failed to get configuration.");
-    let connection_string = configuration.database.connection_string();
-
-    // We have to manually bring Connection trait into scope for pg connection to work
-    let pool = PgPool::connect(&connection_string)
-        .await
-        .expect("Failed to connect to Postgres DB");
-
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     let response = client
-        .post(format!("{}/subscriptions", &app_address))
+        .post(format!("{}/subscriptions", app.address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
@@ -67,7 +69,7 @@ async fn subuscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-        .fetch_one(&pool)
+        .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch savved subscription.");
 
@@ -77,7 +79,7 @@ async fn subuscribe_returns_a_200_for_valid_form_data() {
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
-    let app_address = spawn_app().await;
+    let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let test_cases = vec![
@@ -88,7 +90,7 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
     for (invalid_body, error_message) in test_cases {
         let response = client
-            .post(format!("{}/subscriptions", &app_address))
+            .post(format!("{}/subscriptions", app.address))
             .header("Content-Type", "application/x-www-form-urlencoded")
             .body(invalid_body)
             .send()
