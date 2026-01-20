d5512bf7c37ff5728fe38a64bb73dc16333bfba6
torressam333 <torressam333@gmail.com>
2026-01-20 08:41:46 -0800

    Bring in env_logger for multi dependency logging implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use newsletter_service::configuration::get_configuration;
 use newsletter_service::startup::run;
 use sqlx::PgPool;
@@ -5,6 +6,10 @@ use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
+    // init() calls set_logger
+    // Fall back to printing inifo levl or above for all logs
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Immediately panic if we cant read config
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
