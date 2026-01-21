8a8883149c000290c5680f9d11dee8b4b6ef2c64
torressam333 <torressam333@gmail.com>
2026-01-21 07:59:43 -0800

    Bring in tracing crate to begin adding span structured logging for the api requests

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,12 +10,24 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    /* DEV NOTE: PII & Compliance (GDPR/CCPA)
+    We are logging email and name here for debugging purposes.
+    In a high compliance production environment, this should be avoided
+    or masked to prevent PII leakage into log aggregation systems (e.g. Dynatrace or Datadog).
+    Logs should also be subject to a TTL (Time-To-Live) policy to honor
+    'Right to be Forgotten' requests.
+    */
+    let request_id = Uuid::new_v4();
     log::info!(
-        "Adding '{}' '{}' as a new subscriber.",
+        "Request ID {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
         form.email,
         form.name
     );
-    log::info!("Saving new subscriber to the database.");
+    log::info!(
+        "Request ID {} - Saving new subscriber to the database.",
+        request_id
+    );
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -31,11 +43,18 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .await
     {
         Ok(_) => {
-            log::info!("New subscriber details have been saved");
+            log::info!(
+                "Request ID {} - New subscriber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            log::error!(
+                "Request ID {} - Failed to execute query: {:?}",
+                e,
+                request_id
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
