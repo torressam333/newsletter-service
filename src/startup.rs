b86659981adcd21942c2d615ed9da2b108726301
torressam333 <torressam333@gmail.com>
2026-01-28 08:12:49 -0800

    telemetry: replace standard logger with tracing-actix-web
    - Switch to TracingLogger middleware for structured HTTP context.
    - Remove manual request_id generation from subscribe handler.
    - Enable automatic correlation of logs via root request spans.

--- src/startup.rs
+++ src/startup.rs
@@ -1,9 +1,9 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
-use actix_web::middleware::Logger;
 use actix_web::{App, HttpServer, web};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // Wrap the DB db_pool in a smart pointer
@@ -11,7 +11,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Get a pointer copy and attach to the app's state
