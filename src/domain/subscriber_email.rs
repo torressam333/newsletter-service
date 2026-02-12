c3a875c6ff1b1f032819cbc863e6784ed0a2596a
torressam333 <torressam333@gmail.com>
2026-02-12 08:01:10 -0800

    Bring in crates for more robust testing around email validation

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -1,15 +1,71 @@
+use validator::ValidateEmail;
+
 #[derive(Debug)]
 pub struct SubscriberEmail(String);
 
 impl SubscriberEmail {
     pub fn parse(email: String) -> Result<SubscriberEmail, String> {
-        // Will add valiidation
-        Ok(Self(email))
+        if email.validate_email() {
+            Ok(Self(email))
+        } else {
+            Err(format!("{} is not a valid subscriber email.", email))
+        }
     }
 }
 
 impl AsRef<str> for SubscriberEmail {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+#[cfg(test)]
+mod tests {
+
+    // Use SafeEmail faker and Fake trait to get access to faker's .fake() method
+    use super::SubscriberEmail;
+    use claims::assert_err;
+    use fake::Fake;
+    use fake::faker::internet::en::SafeEmail;
+    use rand::SeedableRng;
+    use rand::rngs::StdRng;
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary(g: &mut quickcheck::Gen) -> Self {
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+
+            let email = SafeEmail().fake_with_rng(&mut rng);
+
+            Self(email)
+        }
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = " ".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "myemail.com".to_string();
+
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = "@something.com".to_string();
+
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn valid_emails_are_parsed_successfully() {
+        let email = SafeEmail().fake();
+
+        claims::assert_ok!(SubscriberEmail::parse(email));
     }
 }
