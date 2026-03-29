132b1ca6821093c30321c55efc79d1e0875e9291
torressam333 <torressam333@gmail.com>
2026-03-29 09:45:47 -0700

    Add unit tests to cover email conf pending confirmation status

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -122,3 +122,43 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
 
     assert_eq!(html_link, text_link);
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app = spawn_app().await;
+    let body = "name=doe%20john&email=john_doe%40gmail.com";
+
+    // This path is for mailtrap, book uses another email provider
+    Mock::given(path_regex(r"^/api/send/\d+"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    let responose = app.post_subscriptions(body.into()).await;
+
+    assert_eq!(200, responose.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_persists_the_new_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=john%20doe&email=john_doe%40gmail.com";
+
+    Mock::given(path_regex(r"^/api/send/\d+"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions")
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription");
+
+    assert_eq!(saved.email, "john_doe@gmail.com");
+    assert_eq!(saved.name, "john doe");
+    assert_eq!(saved.status, "pending_confirmation");
+}
