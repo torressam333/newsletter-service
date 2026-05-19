60af04a03dd47ef8985fe235fcc7012be3bdf3af
torressam333 <torressam333@gmail.com>
2026-05-19 08:04:00 -0700

    Refactor and simplify tests using conf link extractor helper

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,6 +1,5 @@
 use crate::helpers::spawn_app;
 use fake::{Fake, Faker};
-use reqwest::Url;
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, ResponseTemplate};
 
@@ -29,30 +28,11 @@ async fn the_link_returned_by_subscribe_returns_a_200_if_called() {
 
     app.post_subscriptions(body.into()).await;
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
 
-    // Extract the link from one of the request fields
-    let get_link = |s: &str| {
-        let links: Vec<_> = linkify::LinkFinder::new()
-            .links(s)
-            .filter(|l| *l.kind() == linkify::LinkKind::Url)
-            .collect();
+    // Leverage new helper for simpler conf link access
+    let confirmation_links = app.get_confirmation_links(&email_request);
 
-        assert_eq!(links.len(), 1);
-
-        links[0].as_str().to_owned()
-    };
-
-    let raw_confirmation_link = &get_link(&body["HtmlContent"].as_str().unwrap());
-    let mut confirmation_link = Url::parse(raw_confirmation_link).unwrap();
-
-    // Make sure we don't call random api's on the web
-    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
-
-    // Add port to url since tests struggle with dynamic port assignment compared to a prod env
-    confirmation_link.set_port(Some(app.port)).unwrap();
-
-    let response = reqwest::get(confirmation_link).await.unwrap();
+    let response = reqwest::get(confirmation_links.html).await.unwrap();
 
     assert_eq!(response.status().as_u16(), 200);
 }
